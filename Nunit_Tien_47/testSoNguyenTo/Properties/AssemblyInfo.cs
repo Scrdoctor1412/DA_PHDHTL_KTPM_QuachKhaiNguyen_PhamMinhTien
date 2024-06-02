f435344208f42c4646fce0adfe038e546c5899e5
Dau <pmtiena8tqtpy@gmail.com>
2024-06-02 10:26:28 +0700

    first commit

---
+++ Nunit_Tien_47/testSoNguyenTo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testSoNguyenTo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testSoNguyenTo")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd654f0c-4aa2-4c7a-b99c-d034f517dca1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Nunit_Tien_47/testSoNguyenTo/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using soNguyenTo;
+using System;
+
+namespace testSoNguyenTo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\soNT.csv", "soNT#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+       // [ExpectedException(typeof(ArgumentException), "Invalid Data")]
+        public void testSNTCSV()
+        {
+            int n = int.Parse(TestContext.DataRow[0].ToString()); //Chuyển dữ liệu của cột đầu tiên mà nó đọc được thành kiểu dữ liệu int.
+
+            soNguyenToo snt = new soNguyenToo(); //khởi tạo đối tượng snt
+            bool expected =bool.Parse(TestContext.DataRow[1].ToString()); //Chuyển dữ liệu của cột thứ hai mà nó đọc được thành kiểu dữ liệu bool.
+
+            bool actual  = snt.soNT(n);//gọi phương thức soNT của đối tượng snt
+            Assert.AreEqual(expected, actual); //hàm so sánh kết quả mong đợi với kết quả thực tế.
+
+        }
+        [TestMethod]
+        public void testSNT()
+        {
+            int n = 5;
+            soNguyenToo snt = new soNguyenToo(); //khởi tạo đối tượng snt
+            bool actual = snt.soNT(n); //gọi phương thức soNT của đối tượng snt
+            Assert.IsTrue(actual); //hàm kiểm tra true
+        }
+        [TestMethod]
+        public void testSNTF()
+        {
+            int n = -4;
+            soNguyenToo snt = new soNguyenToo();
+            bool actual = snt.soNT(n);
+            Assert.IsFalse(actual);
+        }
+
+    }
+}
